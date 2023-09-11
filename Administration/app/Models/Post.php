687741fe9b781598002ef902a34d0ff387eeab3f
KlaudiaD <132002935+KlaudiaDa@users.noreply.github.com>
2023-09-11 21:00:50 +0200

    Added more models

---
+++ Administration/app/Models/Post.php
@@ -0,0 +1,39 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
+
+class Post extends Model
+{
+    use HasFactory;
+
+    /**
+     * The attributes that are mass assignable.
+     *
+     * @var array<int, string>
+     */
+    protected $fillable = [
+      'user_id',
+      'title',
+      'content',
+      'publication_date'
+    ];
+
+    /**
+     * Get the user that owns the post.
+     */
+    public function user(): \Illuminate\Database\Eloquent\Relations\BelongsTo
+    {
+        return $this->belongsTo(User::class);
+    }
+
+    /**
+     * The tags that belong to the post.
+     */
+    public function tags(): \Illuminate\Database\Eloquent\Relations\BelongsToMany
+    {
+        return $this->belongsToMany(Tag::class);
+    }
+}
